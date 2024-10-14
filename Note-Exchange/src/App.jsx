6d2e98db267c984bf15578c4f328b8b06b2ed033
mihailhr <mishohristov3@gmail.com>
2024-10-14 12:45:11 +0300

    Initial setup

---
+++ Note-Exchange/src/App.jsx
@@ -0,0 +1,16 @@
+// import { Routes,} from "react-router-dom"
+import Navbar from "./Components/Navbar/Navbar"
+
+
+function App() {
+ 
+
+  return (
+    <Navbar/>
+    // <Routes>
+      
+    // </Routes>
+  )
+}
+
+export default App
---
+++ Note-Exchange/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return <nav>
+        <ul>
+            <li>Home</li>
+            <li>Register</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+}
